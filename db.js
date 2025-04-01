891af49300f3b1e1db1709a2ffebc3d753c7d2b0
Jeff Seaman <46333921+jeffreyjseamanppu@users.noreply.github.com>
2025-04-01 08:45:32 -0400

    Update db.js
    Changed the database and host name which were incorrect.

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "api_advanced_programming_user", 
-  host: "dpg-cvccvpij1k6c73c10ou0-a.ohio-postgres.render.com", 
-  database: "Formula 1 project database", 
-  password: "vMWORrD5IdBjjLVS7SX2WDbO2Ue3n3RB", // ✅ Corrected password
+  host: "dpg-cvccvpij1k6c73c10ou0-a", 
+  database: "api_advanced_programming", 
+  password: "vMWORrD5IdBjjLVS7SX2WDbO2Ue3n3RB",
   port: 5432, 
   ssl: { rejectUnauthorized: false } // ✅ Needed for Render connections
 });
